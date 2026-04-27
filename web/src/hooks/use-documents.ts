6e8bba54bce6c8cba77428de4a6f01099191a798
uriva <uriva@users.noreply.github.com>
2026-04-27 22:13:40 +0300

    fix: restore use-documents.ts and properly implement null queries

--- web/src/hooks/use-documents.ts
+++ web/src/hooks/use-documents.ts
@@ -1,5 +1,5 @@
 {
-  "message": "No commit found for the ref 13b9eee7501f83476a09acf7938b1e8ecdfa2a10",
+  "message": "No commit found for the ref f9f886147cfb86c7e981518c310a61af13f823de",
   "documentation_url": "https://docs.github.com/v3/repos/contents/",
   "status": "404"
 }
